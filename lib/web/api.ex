f9fb43d4afbd507784d5c6b15308c328c840a74a
DavidM <toad3k@gmail.com>
2018-03-29 13:34:28 -0400

    Moving api out into its own file.

---
+++ lib/web/api.ex
@@ -0,0 +1,41 @@
+# TODO I'd love to have an openapi specification for this
+# https://github.com/mbuhot/open_api_spex
+defmodule JeopardyWeb.API do
+  use JeopardyWeb, :controller
+
+  @category_num_def "5"
+  @category_num_max "10"
+
+  def random_weighted_categories(conn, params) do
+
+    # TODO investigate simpler argument validation.  Preferably something
+    # that doesn't require ecto.
+    num = (params["num"] || @category_num_def) |>
+      String.to_integer |> min(@category_num_max)
+
+    conn |> success(%{
+      categories: Jeopardy.get_random_weighted_categories(num)
+    })
+  end
+
+  def questions_by_category(conn, params) do
+
+    case Jeopardy.get_category_and_questions(params["category"]) do
+      %{questions: []} -> conn |> failure(404, "No such category")
+      res ->
+        conn |> success(%{
+          category: res.category,
+          questions: res.questions |> Enum.map(&Question.slim/1),
+        })
+    end
+  end
+
+  def success(conn, m) do
+    conn |> json(m |> Map.put(:success, true))
+  end
+
+  def failure(conn, code, message) when is_integer(code) do
+    conn |> put_status(code) |>
+      json(%{success: false, message: message})
+  end
+end
