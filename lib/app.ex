2c0c05bfa1c4b6ca2e588740a98e2da1188b556a
DavidM <toad3k@gmail.com>
2018-03-29 08:39:12 -0400

    Adding web endpoint to application.  Naming top level supervisor.

--- lib/app.ex
+++ lib/app.ex
@@ -1,6 +1,16 @@
-defmodule App.Jeopardy do
+defmodule Jeopardy.Application do
   use Application
+
   def start(_type, _args) do
-    Jeopardy.Supervisor.start_link
+    Supervisor.start_link([
+      Jeopardy.QuestionServer,
+      JeopardyWeb.Endpoint
+    ], strategy: :one_for_one, name: Jeopardy.Supervisor)
   end
+
+#  def config_change(changed, _new, removed) do
+#    Jeopardy.Endpoint.config_change(changed, removed)
+#    :ok
+#  end
+
 end
