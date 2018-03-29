e467772594a40905dc80d21031353c9e9d380cee
DavidM <toad3k@gmail.com>
2018-03-29 08:34:35 -0400

    Base config.  dev and prod configs come separately.

--- config/config.exs
+++ config/config.exs
@@ -28,3 +28,21 @@ use Mix.Config
 # here (which is why it is important to import them last).
 #
 #     import_config "#{Mix.env}.exs"
+
+# Configures the endpoint
+config :jeopardy, JeopardyWeb.Endpoint,
+  url: [host: "localhost"],
+  render_errors: [view: JeopardyWeb.ErrorView, accepts: ~w(html json)]
+
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+
+config :phoenix, :stacktrace_depth, 20
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env}.exs"
